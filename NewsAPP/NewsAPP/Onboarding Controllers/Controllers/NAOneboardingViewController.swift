2ca319263c428957f36e081570ef0a7fcdbba63a
NastyaS <nastya.soiko.97@gmail.com>
2021-04-15 20:14:56 +0300

    added onboardingController, done UICollectionController

---
+++ NewsAPP/NewsAPP/Onboarding Controllers/Controllers/NAOneboardingViewController.swift
@@ -0,0 +1,28 @@
+//
+//  OneboardingViewController.swift
+//  NewsAPP
+//
+//  Created by Настя Сойко on 15.04.21.
+//
+
+import UIKit
+
+class OneboardingController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    @IBOutlet weak var nextButton: UIButton!
+
+    @IBOutlet weak var pageControl: UIPageControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func nextButtonClicked(_ sender: UIButton) {
+
+    }
+
+}
---
+++ NewsAPP/NewsAPP/Onboarding Controllers/Models/NAOnboardingSlide.swift
@@ -0,0 +1,8 @@
+//
+//  NAOnboardingSlide.swift
+//  NewsAPP
+//
+//  Created by Настя Сойко on 15.04.21.
+//
+
+import Foundation
